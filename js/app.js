f9c5bfe11bf309c8732ec1443257a947ffca81f4
Monica <ramirez.92.monica@gmail.com>
2022-09-13 14:33:45 -0500

    created initialization files and folders

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let visitorName()
+
+let question = prompt('do i lve in texas?').toLowerCase();
+if (question ==='y' || === 'yes'){
+  alert ('you are correct');
+}
+else if(){
+  alert();
+}
+let 
