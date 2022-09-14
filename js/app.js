6b7c72e7a73d3fd2a68762b71458412d0f6683c7
Monica <ramirez.92.monica@gmail.com>
2022-09-14 11:36:20 -0500

    added option for user to input y/n with the already exisiting yes/no options, added to all if, else if statements

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ alert (`Nice to meet you, ${visitorName}. Lets play a guessing game so you can g
 
 let birdQuestion= prompt('One of my favorite hobbies is to bird watch, yes or no?').toLowerCase();
 // console.log('if birdQuestion=== yes, correct');//
-if (birdQuestion === 'yes'){
+if (birdQuestion === 'yes' || birdQuestion === 'y'){
   alert('I love birds, I keep track of the birds I\'ve seen.');
 }
-else if(birdQuestion === 'no'){
+else if(birdQuestion === 'no' || birdQuestion === 'n'){
   alert('I do bird watch, my favorite sighting was that of an Indigo Bunting.');
 }else {
   alert('That wasn\'t a yes or no answer. That\'s not fun.');
@@ -17,10 +17,10 @@ let pythonQuestion = prompt('Was Python my first coding language?').toLowerCase(
 // console.log('if pythonQuestion=== yes, correct');//
 
 
-if (pythonQuestion === 'yes'){
+if (pythonQuestion === 'yes' || pythonQuestion === 'y'){
   alert('You are correct, python for the win!');
 }
-else if (pythonQuestion === 'no'){
+else if (pythonQuestion === 'no'|| pythonQuestion ==='n'){
   alert('Python was my first coding language, still learning');
 }
 else{
@@ -31,10 +31,10 @@ let sonQuestion= prompt('Am I a mom?').toLowerCase();
 // console.log('sonQuestion=== yes, correct');//
 
 
-if (sonQuestion === 'yes'){
+if (sonQuestion === 'yes' || sonQuestion === 'y'){
   alert('You\'re right! He\'s is a sweet little person.');
 }
-else if (sonQuestion === 'no'){
+else if (sonQuestion === 'no' || sonQuestion === 'n'){
   alert('I actually have a little human who calls me "mom".');
 }
 else{
@@ -45,10 +45,10 @@ let swimmingQuestion = prompt('Do I know how to swim?').toLowerCase();
 // console.log('if swimmingQuestion=== yes, correct');//
 
 
-if (swimmingQuestion === 'yes'){
+if (swimmingQuestion === 'yes' || swimmingQuestion === 'y'){
   alert('Unfortunately I don\'t know how to swim. I am currently learning.');
 }
-else if (swimmingQuestion === 'no'){
+else if (swimmingQuestion === 'no' || swimmingQuestion === 'n'){
   alert('You are correct! I am learning right now, I just taught myself how to freestyle. Breathing... that I need help with.');
 }
 else{
@@ -58,10 +58,10 @@ else{
 let keyboardQuestion= prompt('Did I go out my way to purchase a loud keyboard for this class?').toLowerCase();
 // console.log('if keyboardQuestion=== yes, correct');//
 
-if (keyboardQuestion === 'yes'){
+if (keyboardQuestion === 'yes' || keyboardQuestion === 'y'){
   alert('You guessed right! I love the clicking clacking sounds.');
 }
-else if (keyboardQuestion === 'no'){
+else if (keyboardQuestion === 'no' || keyboardQuestion === 'n'){
   alert('I did buy a keychron keyboard. The sound of the keys is soothing to me.');
 }
 else{
