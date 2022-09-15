b526bfc0a656e6d64dcc6b5842a09c753c0df20d
Dayo <adedayo789@gmail.com>
2022-09-15 16:41:00 -0500

    Adding quetions for edit

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let visitorName= prompt('Hey, welcome! What\'s your name?')
 //console.log('visitor name')
 alert (`Nice to meet you, ${visitorName}. Lets play a guessing game so you can get to know me! Yes or no responses please.`);
 
+function questionOne (){
 let birdQuestion= prompt('One of my favorite hobbies is to bird watch, yes or no?').toLowerCase();
 // console.log('if birdQuestion=== yes, correct');//
 if (birdQuestion === 'yes' || birdQuestion === 'y'){
@@ -15,7 +16,9 @@ else if(birdQuestion === 'no' || birdQuestion === 'n'){
 }else {
   alert('That wasn\'t a yes or no answer. That\'s not fun.');
 }
+ }
 
+function questionTwo (){
 let pythonQuestion = prompt('Was Python my first coding language?').toLowerCase();
 // console.log('if pythonQuestion=== yes, correct');//
 if (pythonQuestion === 'yes' || pythonQuestion === 'y'){
@@ -28,7 +31,9 @@ else if (pythonQuestion === 'no'|| pythonQuestion ==='n'){
 else{
   alert(`C'mon, ${visitorName}. Humor me.`);
 }
+ }
 
+function questionThree (){
 let sonQuestion= prompt('Am I a mom?').toLowerCase();
 console.log('sonQuestion=== yes, correct');//
 if (sonQuestion === 'yes' || sonQuestion === 'y'){
@@ -41,7 +46,9 @@ else if (sonQuestion === 'no' || sonQuestion === 'n'){
 else{
   alert('There\'s only 2 more, questions left. Hang in there.');
 }
+ }
 
+function questionFour (){
 let swimmingQuestion = prompt('Do I know how to swim?').toLowerCase();
 // console.log('if swimmingQuestion=== yes, correct');//
 if (swimmingQuestion === 'yes' || swimmingQuestion === 'y'){
@@ -54,7 +61,9 @@ else if (swimmingQuestion === 'no' || swimmingQuestion === 'n'){
 else{
   alert('Swimming against the tide here, huh?');
 }
+}
 
+function questionFive (){
 let keyboardQuestion= prompt('Did I go out my way to purchase a loud keyboard for this class?').toLowerCase();
 // console.log('if keyboardQuestion=== yes, correct');//
 if (keyboardQuestion === 'yes' || keyboardQuestion === 'y'){
@@ -67,8 +76,10 @@ else if (keyboardQuestion === 'no' || keyboardQuestion === 'n'){
 else{
   alert('You made it! Just one last message.');
 }
+}
 
 
+function questionSix (){
 let age=30;
 let ageAttempts=4;
 while(ageAttempts>0){
@@ -88,6 +99,15 @@ while(ageAttempts>0){
     alert('All great guesses, but I\'m 30 years old.');
   }
 }
+}
+
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
+questionSix();
+
 
 //question 7 loop, need to fix
 let fruitLoop = ['mango', 'grapefruit', 'watermelon', 'strawberries'];
@@ -105,3 +125,5 @@ for (let i=0; i<6; i++){
   //   alert(`All good guesses, but not quite right. Here are some of my favorite fruits: ${fruitLoop[0]}, ${fruitLoop[1]}, ${fruitLoop[2]}, and ${fruitLoop[3]}. `);
 }
 alert(`Thank for allowing me to share with you, ${visitorName}. You guessed ${userScore} out of 7 answers correctly. If you want to know a little more about me keep reading.`);
+
+
